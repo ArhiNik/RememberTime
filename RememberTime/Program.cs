af46ca4dd382f247f6b6664bb3d69e2dd34f1e7d
Dominik <98458760+ArhiNik@users.noreply.github.com>
2022-07-18 18:51:05 +0300

    Add RememberTime

---
+++ RememberTime/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RememberTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Выполненные задания Ника с учебника ссылка: https://goo.su/EIxr
+            Console.WriteLine("На данный момент выполненны работы: 1.6.; 1.8.; 1.13.; 1.14.; 1.15.;");
+            Console.Write("Введите номер задачи: ");
+            string number = Console.ReadLine();
+
+            switch (number)
+            {
+                case "1.6.":
+                    var oneOutput = new output();
+                    oneOutput.piOutput();
+                    break;
+
+                case "1.8.":
+                    var twoOutput = new output();
+                    twoOutput.readOutput();
+                    break;
+
+                case "1.13.":
+                    var threeOutput = new output();
+                    threeOutput.backOutput();
+                    break;
+                case "1.14.":
+                    var fourOutput = new output();
+                    fourOutput.splitOutput();
+                    break;
+                case "1.15.":
+                    var fiveOutput = new output();
+                    fiveOutput.spOutput();
+                    break;
+
+                default:
+                    Console.WriteLine("Задание не найдено!");
+                    break;
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ RememberTime/output.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RememberTime
+{
+    class output
+    {
+
+        //1.6.
+        public void piOutput()
+        {
+            Console.WriteLine("Число Пи ровна: ");
+            double pi = Math.PI;
+            Console.WriteLine(Math.Round(pi, 3));
+        }
+
+        //1.8.
+        public void readOutput()
+        {
+            Console.Write("Вы ввели число ");
+            int digit = Convert.ToInt32(Console.ReadLine());
+        }
+
+        //1.13.
+        public void backOutput()
+        {
+            Console.Write("Введите число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            int a = b - 1;
+            int c = b + 1;
+
+            Console.Write($"Предыдущее число: {a}\nВпередистоящее число: {c}");
+        }
+
+        //1.14.
+        public void splitOutput()
+        {
+            Console.WriteLine("Введите 3 числа через пробел: ");
+            string str = Console.ReadLine();
+            string[] splitStr = str.Split(' ');
+
+            Console.Write("Вы ввели: ");
+            foreach (var sStr in splitStr)
+            {
+                Console.Write(sStr+" ");
+            }
+
+            Console.ReadKey();
+        }
+
+        //1.15.
+        public void spOutput()
+        {
+            Console.WriteLine("Введите 4 числа через пробел: ");
+            string str = Console.ReadLine();
+            string[] spStr = str.Split(' ');
+
+            Console.Write("Вы ввели: ");
+            foreach (var sStr in spStr)
+            {
+                Console.Write(sStr+" ");
+            }
+        }
+    }
+}
