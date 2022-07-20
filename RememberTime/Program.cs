e6f2119ca8fc50caf176098268eafb8d91dd02dd
Dominik <dadaprostonik@gmail.com>
2022-07-20 10:16:27 +0300

    task 2.1.; 2.2.

--- RememberTime/Program.cs
+++ RememberTime/Program.cs
@@ -8,7 +8,7 @@ namespace RememberTime
         {
 
             //Выполненные задания Ника с учебника ссылка: https://goo.su/EIxr
-            Console.WriteLine("На данный момент выполненны работы: 1.6.; 1.8.; 1.13.; 1.14.; 1.15.; 1.16.;");
+            Console.WriteLine("На данный момент выполненны работы: 1.6.; 1.8.; 1.13.; 1.14.; 1.15.; 1.16.; 2.1.; 2.2.");
             Console.Write("Введите номер задачи: ");
             string number = Console.ReadLine();
 
@@ -39,6 +39,11 @@ namespace RememberTime
                 case "1.16.":
                     var sixOutput = new output();
                     sixOutput.tabOutput();
+                    break;
+
+                case "2.1.":
+                    var oneFrmula = new frmula();
+                    oneFrmula.disFrmula();
                     break;
 
                 default:
--- RememberTime/frmula.cs
+++ RememberTime/frmula.cs
@@ -10,8 +10,18 @@ namespace RememberTime
     {
         //2.1.
         public void disFrmula()
-        { 
-            
+        {
+            Console.Write("Введите х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(17 * x * x - 6 * x + 13);
+        }
+
+        //2.2.
+        public void corFrmula()
+        {
+            Console.Write("Введите a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((a*a + 10)/(Math.Sqrt(a*a+1)));
         }
 
 
